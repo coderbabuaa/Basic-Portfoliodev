3962bcd72dd5b144e7558d003a0a2d501469ce77
Coder Babua <111197936+coderbabuaa@users.noreply.github.com>
2023-10-14 12:18:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    if (mobileMenu.style.display === 'block') {
+        mobileMenu.style.display = 'none';
+    } else {
+        mobileMenu.style.display = 'block';
+    }
+}
